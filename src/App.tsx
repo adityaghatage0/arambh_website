ca96553e1b778c7dc91ef207312ddc88da2e981a
adityaghatage0 <ghatagejyotiraditya@gmail.com>
2025-08-09 11:22:28 +0530

    first push

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Sun } from "lucide-react";
+import ContactUs from "../src/components/ContactUs.tsx";
+
+export default function App() {
+  return <ContactUs />;
+}
